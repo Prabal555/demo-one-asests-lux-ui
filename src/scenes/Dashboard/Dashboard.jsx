3c37a22ad95f5f3cdace40afe3b09e98921aa221
Prabal Raghav <prabalraghav@admins-MacBook-Pro.local>
2020-03-14 03:59:21 +0530

    chores: modularize code and add GLTF exporter

--- src/scenes/Dashboard/Dashboard.jsx
+++ src/scenes/Dashboard/Dashboard.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 import { ElevateAppBar } from '../../components';
 import { Canvas } from './components';
+import { cameraConfig } from '../../config';
 
 class Dashboard extends Component {
     render() {
-        return <ElevateAppBar appName={'Assets-Lux'}><Canvas/></ElevateAppBar>;
+        return (
+            <ElevateAppBar appName={'Assets-Lux'}>
+                <Canvas 
+                    camera={cameraConfig}
+                />
+            </ElevateAppBar>
+        );
     }
 }
 
